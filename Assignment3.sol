4d20e74c398bd1ad034fcaab4ad3111e157fa9b5
aaradhanabisht <67148008+aaradhanabisht@users.noreply.github.com>
2020-07-25 19:53:46 +0530

    Create Assignment3.sol

---
+++ Assignment3.sol
@@ -0,0 +1,19 @@
+pragma solidity >=0.4.14 <0.7.0;
+
+contract Name{
+    
+    string nameOfPerson;
+    
+    function Name(string newName) public {
+        nameOfPerson = newNamer;
+        
+        
+    }function setdetails(string newName) public {
+        nameOfPerson = newNamer;
+        
+        
+    }
+    function getDetails() public view returns(string) {
+        return(nameOfPerson);
+    }
+}
